d28f6d563040b69779f9c733a29128172f27df2f
Charon17th <moonsorrow666.dk@gmail.com>
2018-08-16 01:26:48 +0900

    set await

--- api/index.js
+++ api/index.js
@@ -2,8 +2,18 @@
 
 const ccxt = require('ccxt');
 
-// const handler = async (event) => {
-exports.handler = async (event) => {
+const testdata = {
+  apiKey: 'GDEd5f1DcDzvUOX3q_uI7ecF',
+  secret: '2AyG3wLTAnOW5siuDo0aE-A4RCw5YZ8eS85cER3VaRbIMit-',
+  price: 6549.0,
+  amount: 1,
+  side: 'buy',
+  lossLimit: 5,
+  profitLimit: 10,
+};
+
+const handler = async (event) => {
+// exports.handler = async (event) => {
   //console.log(event);
   
   const exchange = new ccxt.bitmex({
@@ -50,30 +60,17 @@ exports.handler = async (event) => {
     },
   }
 
-  createInOrder(exchange,  inOrder);
-  createOutOrder(exchange, outProfitOrder, outLossOrder);
+  const inres = await createInOrder(exchange,  inOrder);
+  const outres = await createOutOrder(exchange, outProfitOrder, outLossOrder);
 };
 
-const createInOrder = (exchange, inOrder) => {
-  exchange.createOrder(inOrder.symbol, inOrder.orderType, inOrder.side, inOrder.amount, inOrder.price, inOrder.params)
-  .then((res) => {
-    console.log('inOrder');
-    console.log(res);
-  });
+const createInOrder = async (exchange, inOrder) => {
+  return exchange.createOrder(inOrder.symbol, inOrder.orderType, inOrder.side, inOrder.amount, inOrder.price, inOrder.params);
 };
 
-const createOutOrder = (exchange, outProfitOrder, outLossOrder) => {
-	exchange.createOrder(outProfitOrder.symbol, outProfitOrder.orderType, outProfitOrder.side, outProfitOrder.amount, outProfitOrder.price, outProfitOrder.params)
-	.then((res) => {
-		console.log('outProfitOrder');
-		console.log(res);
-	});
-
-	exchange.createOrder(outLossOrder.symbol, outLossOrder.orderType, outLossOrder.side, outLossOrder.amount, outLossOrder.price, outLossOrder.params)
-	.then((res) => {
-		console.log('outLossOrder');
-		console.log(res);
-	});
+const createOutOrder = async (exchange, outProfitOrder, outLossOrder) => {
+	await exchange.createOrder(outProfitOrder.symbol, outProfitOrder.orderType, outProfitOrder.side, outProfitOrder.amount, outProfitOrder.price, outProfitOrder.params);
+	return await exchange.createOrder(outLossOrder.symbol, outLossOrder.orderType, outLossOrder.side, outLossOrder.amount, outLossOrder.price, outLossOrder.params);
 };
 
-//handler(testdata);
+handler(testdata);
