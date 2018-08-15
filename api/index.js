86c91f89d62c88e5569f66ed03f6892f9b5b58c5
Charon17th <moonsorrow666.dk@gmail.com>
2018-08-16 01:07:37 +0900

    update api

--- api/index.js
+++ api/index.js
@@ -2,64 +2,59 @@
 
 const ccxt = require('ccxt');
 
-// ②bitflyer ccxt取得
-let exchange = new ccxt.bitmex({
-  apiKey: '',
-  secret: '',
-  enableRateLimit: true,
-});
+// const handler = async (event) => {
+exports.handler = async (event) => {
+  //console.log(event);
+  
+  const exchange = new ccxt.bitmex({
+    apiKey: event.apiKey,
+    secret: event.secret,
+    enableRateLimit: true,
+  });
 
-const LossLimit = 5;
-const ProfitLimit = 10;
+  const inOrder = {
+    symbol: 'BTC/USD',
+    orderType: 'limit',
+    side: event.side,
+    amount: event.amount,
+    price: event.price,
+    params: {
+      clOrdLinkID: 'in',
+      contingencyType: 'OneTriggersTheOther',
+    },
+  };
 
-const inOrder = {
-  symbol: 'BTC/USD',
-  orderType: 'limit',
-  side: 'sell',
-  amount: 1,
-  price: 6315.0,
-  params: {
-	clOrdLinkID: 'in',
-    contingencyType: 'OneTriggersTheOther',
-  },
+  const outProfitOrder = {
+    symbol: inOrder.symbol,
+    orderType: 'limit',
+    side: inOrder.side === 'buy' ? 'sell' : 'buy',
+    amount: inOrder.amount,
+    price: inOrder.side === 'buy' ? inOrder.price + event.profitLimit : inOrder.price - event.profitLimit,
+    params: {
+      clOrdLinkID: 'in',
+      contingencyType: 'OneCancelsTheOther',
+      execInst: 'ReduceOnly',
+    },
+  }
+
+  const outLossOrder = {
+    symbol: inOrder.symbol,
+    orderType: 'stop',
+    side: inOrder.side === 'buy' ? 'sell' : 'buy',
+    amount: inOrder.amount,
+    params: {
+      clOrdLinkID: 'in',
+      stopPx: inOrder.side === 'buy' ? inOrder.price - event.lossLimit : inOrder.price + event.lossLimit,
+      contingencyType: 'OneCancelsTheOther',
+      execInst: 'ReduceOnly',
+    },
+  }
+
+  createInOrder(exchange,  inOrder);
+  createOutOrder(exchange, outProfitOrder, outLossOrder);
 };
 
-const outProfitOrder = {
-  symbol: inOrder.symbol,
-  orderType: 'limit',
-  side: inOrder.side === 'buy' ? 'sell' : 'buy',
-  amount: inOrder.amount,
-  price: inOrder.side === 'buy' ? inOrder.price + ProfitLimit : inOrder.price - ProfitLimit,
-  params: {
-	clOrdLinkID: 'in',
-    contingencyType: 'OneCancelsTheOther',
-    execInst: 'ReduceOnly',
-  },
-}
-
-const outLossOrder = {
-  symbol: inOrder.symbol,
-  orderType: 'stop',
-  side: inOrder.side === 'buy' ? 'sell' : 'buy',
-  amount: inOrder.amount,
-  params: {
-	clOrdLinkID: 'in',
-	stopPx: inOrder.side === 'buy' ? inOrder.price - LossLimit : inOrder.price + LossLimit,
-    contingencyType: 'OneCancelsTheOther',
-    execInst: 'ReduceOnly',
-  },
-}
-
-const handler = async (event) => {
-// exports.handler = async (event) => {
-  console.log(event);
-  console.log(exchange.has);
-
-  // createInOrder();
-  // createOutOrder();
-};
-
-const createInOrder = () => {
+const createInOrder = (exchange, inOrder) => {
   exchange.createOrder(inOrder.symbol, inOrder.orderType, inOrder.side, inOrder.amount, inOrder.price, inOrder.params)
   .then((res) => {
     console.log('inOrder');
@@ -67,7 +62,7 @@ const createInOrder = () => {
   });
 };
 
-const createOutOrder = () => {
+const createOutOrder = (exchange, outProfitOrder, outLossOrder) => {
 	exchange.createOrder(outProfitOrder.symbol, outProfitOrder.orderType, outProfitOrder.side, outProfitOrder.amount, outProfitOrder.price, outProfitOrder.params)
 	.then((res) => {
 		console.log('outProfitOrder');
@@ -81,20 +76,4 @@ const createOutOrder = () => {
 	});
 };
 
-/*
-(async function () {
-	const res = await exchange.createOrder(symbol, orderType, side, amount, price);
-	console.log(res);
-}) ();
-*/
-
-
-
-/*
-exchange.createOrder(inCancelOrder.symbol, inCancelOrder.orderType, inCancelOrder.side, inCancelOrder.amount, inCancelOrder.price, inCancelOrder.params)
-.then((res) => {
-	console.log('inCancelOrder');
-	console.log(res);
-});
-*/
-
+//handler(testdata);
